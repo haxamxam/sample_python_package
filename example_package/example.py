18b6ca22238a1f106e3c6e37de3330bb18a51930
hamza <hamza.suhail.m@hotmail.com>
2021-09-16 10:25:39 +1000

    changes to set up

--- example_package/example.py
+++ example_package/example.py
@@ -1,6 +1,9 @@
 import pathlib
 
+#use for current working directory
 script_dir = pathlib.Path().resolve()
+
+#if want to check where script is being run
 #script_dir = pathlib.Path(__file__).resolve().parent
 
 def add_one(number):
