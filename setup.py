f4fc7580977b8d19163ebffac394da47bf8e25ed
Hamxa <69680473+haxamxam@users.noreply.github.com>
2021-09-17 14:25:29 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,14 +1,14 @@
 from setuptools import setup
 
-with open("README", 'r') as f:
-    long_description = f.read()
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
 
 setup(
    name='example_package',
    version='0.0.1',
    description='A useful module',
    license="MIT",
-   long_description='long description',
+   long_description=long_description,
    author='Hamza Suhail',
    author_email='foomail@foo.com',
    url="https://github.com/haxamxam/testPackage",
